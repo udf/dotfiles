057486817acd5c7ee420f6811971d012c306db4a
Lonami Exo <totufals@hotmail.com>
2017-08-06 10:48:37 +0200

    Make code more Pythonic
    Get rid of explicit cast to boolean values (i.e. len(data) > 0)
    Slightly different format to make the file more readable

--- .config/polybar/music.py
+++ .config/polybar/music.py
@@ -21,39 +21,45 @@ def cmus_get_filename():
     s = socket(AF_UNIX, SOCK_STREAM)
     s.connect(cmus_get_filename.socket_path)
 
-    if s.send(('status\n').encode('ascii')) == 0:
+    if not s.send(b'status\n'):
         return 'error getting filename'
 
-    ret = s.recv(4096)
+    recv = s.recv(4096)
     s.close()
 
-    ret = re.findall(r'file (.+)\n', ret.decode('utf-8'))
-    if len(ret) <= 0: return ''
+    result = re.findall(r'file (.+)\n', recv.decode('utf-8'))
+    if not result:
+        return ''
 
-    return os.path.splitext(os.path.basename(ret[0]))[0]
-cmus_get_filename.socket_path = os.path.join('/run/', 'user', str(os.getuid()), 'cmus-socket')
+    return os.path.splitext(os.path.basename(result[0]))[0]
+
+cmus_get_filename.socket_path = os.path.join(
+    '/run', 'user', str(os.getuid()), 'cmus-socket'
+)
 
 
 def get_status(player):
     status = player.get_property('status')
-    return {'': '',
-            'Playing': ' ',
-            'Paused': ' ',
-            'Stopped': ' '}.get(status, f'[{status}]')
+    return {
+        '': '',
+        'Playing': ' ',
+        'Paused': ' ',
+        'Stopped': ' '
+    }.get(status, f'[{status}]')
 
 
 def get_position(player, metadata):
     def fmt(tdelta):
         hours, rem = divmod(tdelta.seconds, 3600)
         minutes, seconds = divmod(rem, 60)
-        if hours > 0:
+        if hours:
             return f'{hours:02}:{minutes:02}:{seconds:02}'
         return f'{minutes:02}:{seconds:02}'
 
     position = fmt(timedelta(microseconds=player.get_property('position')))
     duration = metadata.get('mpris:length', 0)
 
-    if duration > 0:
+    if duration:
         return '{}/{}'.format(position, fmt(timedelta(microseconds=duration)))
     return f'{position}'
 
@@ -62,8 +68,8 @@ def get_trackname(player, metadata):
     title = metadata.get('xesam:title', '')
     artist = ', '.join(metadata.get('xesam:artist', ''))
 
-    if artist == '':
-        if title == '' and player.get_property('player-name') == 'cmus':
+    if not artist:
+        if not title and player.get_property('player-name') == 'cmus':
             return cmus_get_filename()
         else:
             return title
@@ -75,7 +81,7 @@ def print_status(player=None, metadata=None):
     output = []
 
     def append_output(data, fmt='{}'):
-        if len(data) > 0:
+        if data:
             output.append(fmt.format(data))
 
     global current_player
